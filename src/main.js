395aad22d4afcc2e60a85ddea1bfd2c1184d6efb
Antoine Jaunard <antoine.stuff@pm.me>
2022-05-19 23:17:15 +0200

    chart implementation wip

--- src/main.js
+++ src/main.js
@@ -1,23 +1,39 @@
 import './index.css'
-import settings from './api/settings.json'
-import kits from './api/kits.json'
+import Settings from './api/settings.json'
+import Kits from './api/kits.json'
+import Chart from 'chart.js/auto';
+
+const app = document.getElementById("app");
+let mainKit, dateStart, dateEnd;
 
 window.onload = function () {
   select();
+  getDates();
 };
 
 function select() {
   // select
   let select = document.getElementById('select');
   if (select) select.addEventListener('change', function(event) {
-    getKit(event.target.value)
+    mainKit = event.target.value;
+    getKit(mainKit)
   });
-  for (let i in kits) {
+  for (let i in Kits) {
     let opt = document.createElement('option');
-    opt.value = kits[i].id;
-    opt.innerHTML = kits[i].name;
+    opt.value = Kits[i].id;
+    opt.innerHTML = Kits[i].name;
     select.appendChild(opt);
   }
+}
+
+function getDates() {
+  // get date
+  dateEnd = new Date(new Date().getTime());
+  dateStart = new Date(new Date().getTime());
+  dateStart.setDate(new Date().getDate() - 7);
+  // format date
+  dateStart = dateStart.toISOString().split('T')[0];
+  dateEnd = dateEnd.toISOString().split('T')[0];
 }
 
 function getKit(id) {
@@ -32,8 +48,8 @@ function getKit(id) {
 }
 
 function displayKit(kit) {
-  const app = document.getElementById("app");
   app.innerHTML = "";
+  // sensors
   for (let i in kit.data.sensors) {
     let sensor = kit.data.sensors[i];
     let elem = document.createElement("article");
@@ -46,4 +62,45 @@ function displayKit(kit) {
     `
     app.appendChild(elem);
   }
+  // chart
+  for (let i in Settings.sensorsSelection) {
+    let sensor = Settings.sensorsSelection[i];
+    const sensorUrl = `https://api.smartcitizen.me/v0/devices/${mainKit}/readings?sensor_id=${sensor}&rollup=4h&from=${dateStart}&to=${dateEnd}`;
+    https: fetch(sensorUrl)
+    .then((res) => {
+      return res.json();
+    })
+    .then((sensor) => {
+      displayChart(sensor);
+    });
+  }
+}
+
+function displayChart(sensor) {
+  let dataStruct = [];
+  for (const reading of sensor.readings) {
+    let dataItem = {
+      x: reading[0],
+      y: reading[1],
+    };
+    dataStruct.push(dataItem);
+  }
+  let chart = document.createElement("canvas");
+  app.appendChild(chart);
+  const myChart = new Chart(chart, {
+    type: 'bar',
+    data: {
+      datasets: [{
+        label: '# of Votes',
+        data: dataStruct,
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 }
